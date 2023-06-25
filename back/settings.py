96ea008459557bab6a11aadee374992619018351
illi-homz <gopkabel@mail.ru>
2023-06-25 18:21:36 +0300

    fix staticfiles

--- back/settings.py
+++ back/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/mediafiles/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "api/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
